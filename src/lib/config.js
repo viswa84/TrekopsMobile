8f3dc00649d5f1ac31b32f0ef23ef7c826a9d618
viswa84 <viswavishwanath5@gmail.com>
2026-05-24 19:59:39 +0530

    Fix network error on mobile by using local IP instead of localhost

--- src/lib/config.js
+++ src/lib/config.js
@@ -4,9 +4,9 @@ const extra = Constants.expoConfig?.extra ?? {};
 
 export const config = {
   graphqlUrl:
-    process.env.EXPO_PUBLIC_GRAPHQL_URL || extra.graphqlUrl || 'http://localhost:8080/graphql',
+    process.env.EXPO_PUBLIC_GRAPHQL_URL || extra.graphqlUrl || 'http://192.168.1.22:8080/graphql',
   socketUrl:
-    process.env.EXPO_PUBLIC_SOCKET_URL || extra.socketUrl || 'http://localhost:8080',
+    process.env.EXPO_PUBLIC_SOCKET_URL || extra.socketUrl || 'http://192.168.1.22:8080',
   apiBaseUrl:
-    process.env.EXPO_PUBLIC_API_BASE_URL || extra.apiBaseUrl || 'http://localhost:8080',
+    process.env.EXPO_PUBLIC_API_BASE_URL || extra.apiBaseUrl || 'http://192.168.1.22:8080',
 };
